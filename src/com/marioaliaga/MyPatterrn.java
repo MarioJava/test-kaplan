f7ed50f4cfbd9c6d873e92995f6a8b81f1aefb30
Mario Aliaga <maliaga@nisum.com>
2016-07-14 09:08:45 -0400

    Initial commit

---
+++ src/com/marioaliaga/MyPatterrn.java
@@ -0,0 +1,21 @@
+package com.marioaliaga;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by maliaga on 14-07-16.
+ */
+public class MyPatterrn {
+    public static void main(String[] args) {
+        String line = "Financial Year 2012 - 2013";
+        String pattern ="\\d+";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(line);
+        if (m.find()) {
+            System.out.println("Found value: " + m.group(0));
+        } else {
+            System.out.println("NOT FOUND");
+        }
+    }
+}
---
+++ src/com/marioaliaga/StringSplit.java
@@ -0,0 +1,16 @@
+package com.marioaliaga;
+
+/**
+ * Created by maliaga on 14-07-16.
+ */
+public class StringSplit {
+
+    public static void main(String... args){
+        String s = "x1234 y56 z7 a";
+        String [] sa = s.split("\\d");
+        int count = 0;
+        for( String x : sa)
+            count++;
+        System.out.println("total: " + count);
+    }
+}
